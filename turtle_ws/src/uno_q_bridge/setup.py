def160a49d2a59c302662b0c3b71118daca37b81
jesus.loport <jesus.loport@outlook.com>
2026-03-28 00:18:54 +0000

    ROS 2 & arduino-router.sock successfull integration

--- turtle_ws/src/uno_q_bridge/setup.py
+++ turtle_ws/src/uno_q_bridge/setup.py
@@ -11,7 +11,10 @@ setup(
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools'],
+    install_requires=[
+        'setuptools',
+        'msgpack'
+    ],
     zip_safe=True,
     maintainer='jesus.loport',
     maintainer_email='jesus.loport@outlook.com',
@@ -24,6 +27,7 @@ setup(
     },
     entry_points={
         'console_scripts': [
+            'uno_q_bridge = uno_q_bridge.uno_q_bridge:main',
         ],
     },
 )
